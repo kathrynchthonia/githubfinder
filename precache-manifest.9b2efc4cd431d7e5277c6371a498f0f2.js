aba47e94eda4e7c1f292e633d0a45149840ccd0c
twheelertech <tony@wheelertechconsulting.com>
2019-10-06 07:56:22 -0500

    first commit

---
+++ precache-manifest.9b2efc4cd431d7e5277c6371a498f0f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8441dd7af4136900af52213b12cda41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c9d6b75a72f2f0bd47d",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "ae13f0888c87fcc36f84",
+    "url": "/static/js/2.35e62f78.chunk.js"
+  },
+  {
+    "revision": "9c9d6b75a72f2f0bd47d",
+    "url": "/static/js/main.8e5c445a.chunk.js"
+  },
+  {
+    "revision": "91f382234f7437305a32",
+    "url": "/static/js/runtime~main.f8fad88d.js"
+  }
+]);
